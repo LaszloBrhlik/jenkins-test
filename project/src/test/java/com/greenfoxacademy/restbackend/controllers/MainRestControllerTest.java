dc8fa8c53f5dcc69bb953aefa991ff320f3c0038
Laszlo Brhlik <brhlik.laszlo@gmail.com>
2018-05-17 13:31:06 +0200

    feat(MainRestControllerTest): failed test

--- project/src/test/java/com/greenfoxacademy/restbackend/controllers/MainRestControllerTest.java
+++ project/src/test/java/com/greenfoxacademy/restbackend/controllers/MainRestControllerTest.java
@@ -20,7 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class MainRestControllerTest {
 
   @Autowired
-  private MockMvc mockMvc;
+  private MockMvc mockMv;
 
   // doubling
   @Test
